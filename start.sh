c76a4aed7003fde192dfcd5c36dad4ce9aae1b44
Rohit Jha <rjha@ph.iitr.ac.in>
2021-03-27 02:39:24 +0530

    feat: docker start file

---
+++ start.sh
@@ -0,0 +1,10 @@
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+echo "Starting Server"
+gunicorn --workers=5 --bind 0.0.0.0:8000 backend.wsgi
